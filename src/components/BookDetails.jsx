c03bb8385cd3fde2acaab9a4bf6a018941d041db
David Mortkowitz <dmortkowitz@gmail.com>
2018-12-07 15:43:09 -0800

    Add additional progress to collapsible book cards. The title and author are now clickable, but the content does not hide or show yet.

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -14,15 +14,18 @@ function BookDetails(props){
         <div id='accordian'>
           <div className='card'>
             <div className='mb-0'>
-              <button className='btn btn-link' data-toggle collapse' data-target='#collapseOne' aria-expanded='true' aria-controls='collapseOne'>
-            <h3>{props.author} - {props.title}</h3>
+              <button className='btn btn-link' data-toggle='collapse' data-target='#collapseOne' aria-expanded='true' aria-controls='collapseOne'>
+                <h3>{props.author} - {props.title}</h3>
+              </button>
+            </div>
+          </div>
             <p><em>{props.description}</em></p>
-            <p>{props.pages} pages.</p>
+            <p>This book is {props.pages} pages.</p>
             <hr/>
           </div>
         </div>
       </div>
-    </div>
+
   );
 }
 
