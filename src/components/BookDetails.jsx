1c4c1f4800bfdee1c4fea6ce3786a151884db401
David Mortkowitz <dmortkowitz@gmail.com>
2018-12-07 16:39:31 -0800

    Cancel failed experiment for click-show.

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import ToggleDisplay from 'react-toggle-display';
 import PropTypes from 'prop-types';
+
+
 
 class BookDetails extends React.Component {
   constructor() {
     super();
+    this.state = { show: false };
   }
 
-
+  handleClick() {
+    this.setState({
+      show: !this.state.show
+    });
+  }
 
   render(){
     return (
@@ -23,13 +31,15 @@ class BookDetails extends React.Component {
           <div id='accordian'>
             <div className='card'>
               <div className='mb-0'>
-                <button className='btn btn-link'>
+                <button onClick={ () => this.handleClick() }>
                   <h3>{props.author} - {props.title}</h3>
                 </button>
               </div>
             </div>
-            <span id='contentHide'><p><em>{props.description}</em></p>
-              <p>This book is {props.pages} pages.</p></span>
+            <span id='contentHide'>
+              <p><em>{props.description}</em></p>
+              <p>This book is {props.pages} pages.</p>
+            </span>
             <hr/>
           </div>
         </div>
